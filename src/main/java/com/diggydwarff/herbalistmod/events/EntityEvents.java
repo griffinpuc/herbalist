1118378ee5654f5bd419db3a1086dd1c74791429
Griffin Puc <griffinpuc@outlook.com>
2024-07-17 20:04:04 -0400

    More changes

--- src/main/java/com/diggydwarff/herbalistmod/events/EntityEvents.java
+++ src/main/java/com/diggydwarff/herbalistmod/events/EntityEvents.java
@@ -14,63 +14,6 @@ import net.minecraftforge.network.PacketDistributor;
 
 public class EntityEvents {
 
-    @SubscribeEvent
-    public void onClick(PlayerInteractEvent.EntityInteractSpecific event) {
-        if (!event.getLevel().isClientSide) {
-            return;
-        }
 
-        Player player = event.getEntity();
-
-        if(event.getHand().toString() == "MAIN_HAND"){
-
-            // IF FROG GIVE FROG VENOM
-            if ((event.getTarget() instanceof Frog)) {
-                Frog frog = (Frog) event.getTarget();
-                if(player.isHolding(ModItems.GLASS_VIAL.get())){
-                    int glassVialCount = Lists.newArrayList(player.getHandSlots()).get(0).getCount();
-                    if(glassVialCount == 1){
-                        player.getInventory().setItem(player.getInventory().selected, new ItemStack(ModItems.FROG_VENOM.get()));
-                    } else {
-                        int freeSlotId = player.getInventory().getFreeSlot();
-                        if(freeSlotId >= 0){
-                            player.getInventory().setItem(freeSlotId, new ItemStack(ModItems.FROG_VENOM.get()));
-                        } else{
-                            return;
-                        }
-                    }
-                }
-                return;
-            }
-
-            // IF AXOLOTL GIVE AXOLOTL VENOM
-            else if ((event.getTarget() instanceof Axolotl)) {
-                Axolotl axolotl = (Axolotl) event.getTarget();
-                if(player.isHolding(ModItems.GLASS_VIAL.get())){
-                    if(Lists.newArrayList(player.getHandSlots()).get(0).getCount() == 1){
-                        int freeSlotId = player.getInventory().getFreeSlot();
-                        if(freeSlotId >= 0){
-                            player.getInventory().setItem(freeSlotId, new ItemStack(ModItems.FROG_VENOM.get()));
-                        } else{
-                            return;
-                        }
-                    } else {
-                        int freeSlotId = player.getInventory().getFreeSlot();
-                        if(freeSlotId >= 0){
-                            player.getInventory().setItem(freeSlotId, new ItemStack(ModItems.FROG_VENOM.get()));
-                        } else{
-                            return;
-                        }
-                    }
-                }
-                return;
-            }
-        }
-
-        NetUtils.sendToServer(Main.SIMPLE_CHANNEL, new MessageSyncInventory(player.getUUID()));
-
-        event.setCancellationResult(InteractionResult.SUCCESS);
-        event.setCanceled(true);
-    }
 
 }
